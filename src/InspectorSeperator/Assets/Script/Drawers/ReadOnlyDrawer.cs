a2668248f2fae501f6e16e2c906efe6b3b37e075
Arni Asgeirsson <arnidat@gmail.com>
2014-11-25 01:41:03 +0100

    ReadOnlyAttribute and Drawer
    Implemented an attribute for marking a variable readonly meaning it
    cannot be edited through the inspector but only the script.

---
+++ src/InspectorSeperator/Assets/Script/Drawers/ReadOnlyDrawer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomPropertyDrawer (typeof (ReadOnlyAttribute))]
+public class ReadOnlyDrawer : PropertyDrawer {
+
+	ReadOnlyAttribute _attribute { get { return ((ReadOnlyAttribute)attribute); } }
+
+	// Here you must define the height of your property drawer. Called by Unity.
+	public override float GetPropertyHeight (SerializedProperty prop, GUIContent label) {
+		
+		return base.GetPropertyHeight (prop, label);
+	}
+
+	public override void OnGUI (Rect position, SerializedProperty prop, GUIContent label) {
+
+		// TODO: Update the prop such that it holds the value of the script variable?
+
+		GUI.enabled = false;
+		EditorGUI.PropertyField(position, prop, label);
+		GUI.enabled = true;
+	}
+}
--- src/InspectorSeperator/Assets/Script/SeparatorTest.cs
+++ src/InspectorSeperator/Assets/Script/SeparatorTest.cs
@@ -3,12 +3,21 @@ using System.Collections;
 
 public class SeparatorTest : MonoBehaviour {
 
-	public float a;
+	[ReadOnlyAttribute]
+	public float a = 1245.8f;
+
+	[ReadOnlyAttribute]
+	public float aaa = 1245.8f;
 	[SpaceAttribute (25)]
 	public float b;
 	[SeparatorAttribute (8.0f, 0.8f)]
 	public float c;
-	public float d;
+	[ReadOnly]
+	public float d = 124;
+	
+	[ReadOnlyAttribute]
+	[SerializeField]
+	private float e = 1288;
 
 	// Use this for initialization
 	void Start () {
