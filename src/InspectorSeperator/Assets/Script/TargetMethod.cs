e28afb1b1733ac039e4eb5a71a4718e23b1aa0dd
Arni Asgeirsson <arnidat@gmail.com>
2014-11-26 14:03:53 +0100

    Added TargetMethod for holding a callable target method reference

---
+++ src/InspectorSeperator/Assets/Script/TargetMethod.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class TargetMethod {
+	public GameObject target;
+	public Component component;
+	public string method;
+	public List<Object> args;
+
+	public TargetMethod () {
+		target = null;
+		component = null;
+		method = "";
+		args = new List<Object>();
+	}	
+}
