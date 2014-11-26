49c05f82111889f319871a247d75760e0dc713b7
Arni Asgeirsson <arnidat@gmail.com>
2014-11-26 16:38:43 +0100

    Added property and drawer for restricting the values of a vector between it self.

---
+++ src/InspectorSeperator/Assets/Script/Drawers/MinMaxDrawer.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System;
+
+[CustomPropertyDrawer (typeof (MinMaxAttribute))]
+public class MinMaxDrawer : PropertyDrawer {
+
+	private Vector2 _prevVector2 = new Vector2(0,0);
+	private Vector3 _prevVector3 = new Vector3(0,0,0);
+	private Vector4 _prevVector4 = new Vector4(0,0,0,0);
+
+	private bool _firstTime = true;
+
+	MinMaxAttribute _attribute { get { return ((MinMaxAttribute)attribute); } }
+
+	// Here you must define the height of your property drawer. Called by Unity.
+	public override float GetPropertyHeight (SerializedProperty prop, GUIContent label) {
+		
+		return EditorGUI.GetPropertyHeight (prop, label, true);
+	}
+	
+	public override void OnGUI (Rect position, SerializedProperty prop, GUIContent label) {
+
+		string proptype = prop.type;
+
+		
+
+		// TODO: Use a more proper way of comparing the types..
+		if (proptype == (typeof(Vector2).Name+"f")) {
+
+			Vector2 val = prop.vector2Value;
+
+			if (_firstTime) {
+
+				val.y = val.x > val.y ? val.x : val.y;
+
+				_firstTime = false;
+			} else {
+
+				if (!Mathf.Approximately(val.x, _prevVector2.x) && val.x > val.y) {
+					val.x = val.y;
+				} else if (!Mathf.Approximately(val.y, _prevVector2.y) && val.x > val.y) {
+					val.y = val.x;
+				}
+			}
+			_prevVector2 = val;
+			prop.vector2Value = val;
+
+			EditorGUI.PropertyField(position, prop, label);
+		} else if (proptype == (typeof(Vector3).Name+"f")) {
+			
+			Vector3 val = prop.vector3Value;
+			
+			if (_firstTime) {
+				
+				val.y = val.x > val.y ? val.x : val.y;
+				val.z = val.y > val.z ? val.y : val.z;
+
+				_firstTime = false;
+			} else {
+				
+				if (!Mathf.Approximately(val.x, _prevVector3.x) && val.x > val.y) {
+					val.x = val.y;
+				} else if (!Mathf.Approximately(val.y, _prevVector3.y) && val.y < val.x) {
+					val.y = val.x;
+				} else if (!Mathf.Approximately(val.y, _prevVector3.y) && val.y > val.z) {
+					val.y = val.z;
+				} else if (!Mathf.Approximately(val.z, _prevVector3.z) && val.z < val.y) {
+					val.z = val.y;
+				}
+			}
+			_prevVector3 = val;
+			prop.vector3Value = val;
+			
+			EditorGUI.PropertyField(position, prop, label);
+		} else if (proptype == (typeof(Vector4).Name+"f")) {
+			
+			Vector4 val = prop.vector4Value;
+			
+			if (_firstTime) {
+				
+				val.y = val.x > val.y ? val.x : val.y;
+				val.z = val.y > val.z ? val.y : val.z;
+				val.w = val.z > val.w ? val.z : val.w;
+
+				_firstTime = false;
+			} else {
+				
+				if (!Mathf.Approximately(val.x, _prevVector4.x) && val.x > val.y) {
+					val.x = val.y;
+				} else if (!Mathf.Approximately(val.y, _prevVector4.y) && val.y < val.x) {
+					val.y = val.x;
+				} else if (!Mathf.Approximately(val.y, _prevVector4.y) && val.y > val.z) {
+					val.y = val.z;
+				} else if (!Mathf.Approximately(val.z, _prevVector4.z) && val.z < val.y) {
+					val.z = val.y;
+				} else if (!Mathf.Approximately(val.z, _prevVector4.z) && val.z > val.w) {
+					val.z = val.w;
+				} else if (!Mathf.Approximately(val.w, _prevVector4.w) && val.w < val.z) {
+					val.w = val.z;
+				}
+			}
+			_prevVector4 = val;
+			prop.vector4Value = val;
+			
+			EditorGUI.PropertyField(position, prop, label, true);
+		} else {
+			EditorGUI.LabelField(position, label, new GUIContent("Use MinMax with Vectors."));
+		}
+	}
+}
