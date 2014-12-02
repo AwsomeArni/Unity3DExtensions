d913edff19ebf8e05eb44791e2d15b74c1e0af16
Arni Asgeirsson <arnidat@gmail.com>
2014-12-02 14:17:00 +0100

    #24 Added an attribute for forcing a regex on a string

---
+++ src/InspectorSeperator/Assets/Script/Attributes/RegexAttribute.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RegexAttribute : PropertyAttribute {
+
+	public string regex;
+	public string defaultValue;
+	public string helpMessage;
+
+	public RegexAttribute (string regex) {
+		this.regex = regex;
+		this.defaultValue = "String does not match regex.";
+		this.helpMessage = "String must match regex: "+regex;
+	}
+
+	public RegexAttribute (string regex, string defaultValue) {
+		this.regex = regex;
+		this.defaultValue = defaultValue;
+		this.helpMessage = "String must match regex: "+regex;
+	}
+
+	public RegexAttribute (string regex, string defaultValue, string helpMessage) {
+		this.regex = regex;
+		this.defaultValue = defaultValue;
+		this.helpMessage = helpMessage;
+	}
+}
---
+++ src/InspectorSeperator/Assets/Script/Drawers/RegexDrawer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+[CustomPropertyDrawer (typeof (RegexAttribute))]
+public class RegexDrawer : PropertyDrawer {
+
+	RegexAttribute _attribute { get { return ((RegexAttribute)attribute); } }
+	
+	// Here you must define the height of your property drawer. Called by Unity.
+	public override float GetPropertyHeight (SerializedProperty prop, GUIContent label) {
+		
+		return EditorGUI.GetPropertyHeight (prop, label, true);
+	}
+	
+	public override void OnGUI (Rect position, SerializedProperty prop, GUIContent label) {
+
+		EditorGUI.BeginChangeCheck ();
+		position.width -= 18;
+		string value = EditorGUI.TextField (position, label, prop.stringValue);
+		if (EditorGUI.EndChangeCheck ()) {
+			prop.stringValue = value;
+		}
+
+		GUI.enabled = false;
+		position.x = EditorGUIUtility.currentViewWidth - 18;
+		EditorGUI.Toggle(position, IsValid(prop));
+		GUI.enabled = true;
+	}
+
+	// Test if the propertys string value matches the regex pattern.
+	bool IsValid (SerializedProperty prop) {
+		return Regex.IsMatch (prop.stringValue, _attribute.regex);
+	}
+}
