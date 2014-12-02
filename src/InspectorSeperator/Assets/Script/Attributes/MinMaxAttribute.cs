dd7f697182f28a8fd39d8f8bc4991a329b0c55a0
Arni Asgeirsson <arnidat@gmail.com>
2014-12-02 13:31:06 +0100

    Some weird name change fix..

---
+++ src/InspectorSeperator/Assets/Script/Attributes/MinMaxAttribute.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinMaxAttribute : PropertyAttribute {
+
+	public bool considerMin = false;
+	public float min = 0;
+	public bool considerMax = false;
+	public float max = 0;
+
+	public MinMaxAttribute() {
+		this.considerMin = false;
+		this.considerMax = false;
+	}
+
+	public MinMaxAttribute(float min, float max) {
+		if (!Mathf.Approximately(Mathf.Infinity, Mathf.Abs(min))) {
+			this.min = min;
+			this.considerMin = true;
+		} else {
+			this.considerMin = false;
+		}
+		if (!Mathf.Approximately(Mathf.Infinity, Mathf.Abs(max))) {
+			this.max = max;
+			this.considerMax = true;
+		} else {
+			this.considerMax = false;
+		}
+	}
+
+}
